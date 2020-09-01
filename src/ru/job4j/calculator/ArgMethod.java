4a1e1e6f7aa9227bdbb1885225ddea48d1c83af2
maliafka <kiulika0@gmail.com>
2020-09-01 22:47:45 +0300

    4.1. Аргументы [#346216]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -8,9 +8,9 @@ public class ArgMethod {
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        int age = 33;
+        String age = "33";
 
         ArgMethod.hello(name);
-
+        ArgMethod.hello(age);
     }
 }
